1b6466c00f9e943b06027378d1c55a2624cd8234
SanjuJssmr <106820541+SanjuJssmr@users.noreply.github.com>
2022-11-23 16:45:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import '@ionic/react/css/core.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
